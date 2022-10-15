815e2c76cf988251999736b804b27129f0a11dbb
wxl-922 <78382587+wxl-922@users.noreply.github.com>
2022-10-15 19:47:58 -0400

    Add files via upload

---
+++ app/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from user_data.listing import bp as listing_bp
+
+app = Flask(__name__)
+app.register_blueprint(listing_bp)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template('main_page.html')
+
+
+@app.route('/listing.js', methods=['GET'])
+def send_js():
+    with open('front_end/listing.js', 'rb') as js:
+        return js.read()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
