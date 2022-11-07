7c56e9192545043aefad69e63a074c2508ddce78
MaRonggg <74996996+MaRonggg@users.noreply.github.com>
2022-11-07 15:07:50 -0500

    Add files via upload

---
+++ front_end/cart.js
@@ -0,0 +1,44 @@
+$('#Submit').click(function() {
+
+    const Account = $('#CartAccount').val();
+    const Password = $('#CartPassword').val();
+
+    if (Account.length > 0 && Password.length > 0 ) {
+            const formData = new FormData();
+            formData.append('id', Account);
+            $.ajax({
+            url: '/checkinfor',
+            method: 'GET',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                eppass= res["password"]
+                }
+            });
+        if (Password===eppass) {
+            $.ajax({
+                url: '/showcart',
+                method: 'GET',
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function (res) {
+
+                    $('#CartAccount').val('');
+                    $('#CartPassword').val('');
+                }
+            })
+        } else {
+            document.getElementById('error').innerHTML = 'Incorrect Account or Password';
+
+        }
+    } else {
+        document.getElementById('error').innerHTML = 'Please provide a valid account and password';
+
+    }
+})
+
+
+
+
