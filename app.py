ae4e578e4a8d5d6c3e00d6f63882779c36e00a58
wxl-922 <xwu46@buffalo.edu>
2022-11-18 15:59:02 -0500

    update app

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
+from pathlib import Path
+
 from flask import Flask, render_template
 from user_data.listing import bp as listing_bp
 from user_data.cart import cartbp as cart_bp
 from flask import Flask, request
+
+
 app = Flask(__name__)
 app.register_blueprint(listing_bp)
 app.register_blueprint(cart_bp)
 
 
 @app.route('/', methods=['GET'])
-def hello_world():
+def main_page():
     return render_template('main_page.html')
 
 
 @app.route('/listing.js', methods=['GET'])
 def send_js():
-    with open('front_end/listing.js', 'rb') as js:
+    with open(Path(__file__).parent / 'front_end/listing.js', 'rb') as js:
         return js.read()
+
 
 @app.route('/cart.js', methods=['GET'])
 def send_js1():
-    with open('front_end/cart.js', 'rb') as js:
+    with open(Path(__file__).parent / 'front_end/cart.js', 'rb') as js:
         return js.read()
-
 
 
 if __name__ == '__main__':
