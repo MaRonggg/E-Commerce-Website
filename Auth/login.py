44f20483c3c83546b373f0d250bfd31db4d0fd93
Steve li <happystevenli9755@gmail.com>
2022-11-21 13:06:45 -0500

    login.html update

--- Auth/login.py
+++ Auth/login.py
@@ -1,3 +1,9 @@
 from flask import render_template
 from flask import request
 from flask import session
+
+
+# in here, we need to login and salted the token for the user
+
+# when success, we need to
+
