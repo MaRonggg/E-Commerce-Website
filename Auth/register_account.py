7115ff6fbda1ae8b8c93253c4e6e4aead72eef9c
yutianluo <70578924+yutianluo@users.noreply.github.com>
2022-11-21 16:22:32 -0500

    update

--- Auth/register_account.py
+++ Auth/register_account.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, request, render_template
+import bcrypt
+from database import database
 import json
 from database.database import get_one_user, get_one_shopping_cart
 import  database.database as db
 # from database import db
-
+salt = bcrypt.gensalt()
 register = Blueprint('register', __name__)
 
 
@@ -17,12 +19,14 @@ def reg():
     else:
         # after submit form, store the value into database
         # use the lab for id to find the part
-        username = request.form.get("uname")
-        password = request.form.get("psw")
+        email = request.form.get("email")
+        password = bcrypt.hashpw(request.form.get("psw").encode('utf-8'), salt)
+        name = request.form.get("name")
         print(f'reg_info being called')
         print(request)
-        print(f'username would be {username}')
+        print(f'username would be {name}')
         print(f'password would be {password}')
+        database.create_user_account(email, password, name)
 
 
 # after submit form, store the value into database
