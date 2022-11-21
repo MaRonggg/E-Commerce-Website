6196427330d63d339726ba2a973c6fcbee0f11ac
yutianluo <70578924+yutianluo@users.noreply.github.com>
2022-11-21 12:11:59 -0500

    update login

--- Auth/login.py
+++ Auth/login.py
@@ -1,3 +1,13 @@
+import flask
 from flask import render_template
 from flask import request
 from flask import session
+
+auth = flask.Blueprint("auth",__name__)
+
+@auth.route("/login",methods=['GET','POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+
