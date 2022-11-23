243c1f762217cd6b67a5574948f523c1714b7115
yutianluo <70578924+yutianluo@users.noreply.github.com>
2022-11-23 00:00:51 -0500

    update

--- Auth/login.py
+++ Auth/login.py
@@ -18,15 +18,15 @@ def login():
             return redirect(url_for('login'))
         user = database.user_collection.find_one({"email": email})
         if not user:
-            flash('Invalid email.')
+            flash('Email not Found.')
             return redirect(url_for('login'))
         if not checkpw(hashpw(password.encode('utf-8'), salt), user.get("password")):
             flash('Incorrect password.')
             return redirect(url_for('login'))
         else:
-
+            session["email"] = email
             flash('Login success.')
-            return redirect(url_for('/'))
+            return render_template("logined.html", email=email)
     return render_template("login.html")
 
 
