90dab37549e48b682bbe0b862d4ed7e33c738f57
MaRonggg <74996996+MaRonggg@users.noreply.github.com>
2022-11-27 00:25:17 -0500

    Update cart.py

--- user_data/cart.py
+++ user_data/cart.py
@@ -1,39 +1,61 @@
 from flask import Blueprint, request, render_template
-import json
-from database.database import get_one_user, get_one_shopping_cart
 
-#from database import db
+from database.database import get_one_user, get_one_shopping_cart,create_shopping_cart,get_one_product, remove_product_from_shopping_cart
+from flask import render_template, session
+
 
 cartbp = Blueprint('cart', __name__)
-#listing_collection = db['listing']
+
 
 
 @cartbp.route('/shopping_cart', methods=['GET'])
 def direct():
-    return render_template('cart.html')
+    if "email" in session:
+        email = session['email']
+        image_list = []
+        if get_one_shopping_cart(email) is None:
+            create_shopping_cart(email)
+
+        else:
+            a = get_one_shopping_cart(email)["product_id_list"]
+            for aid in a:
+                image_list.append(get_one_product(aid)["product_image"])
+        return render_template(
+            'user_cart.html',
+            nav=image_list
+        )
+    else:
+        return render_template('login.html')
+
+
+@cartbp.route('/delete_from_cart', methods=['POST'])
+def delete_item(product_image):
+    aid = get_one_product_by_imagename(product_image)["_id"]
+    email = session['email']
+    remove_product_from_shopping_cart(aid, user_email=email)
+    a = get_one_shopping_cart(email)["product_id_list"]
+    image_list = []
+    for aid in a:
+        image_list.append(get_one_product(aid)["product_image"])
+    return render_template(
+        'user_cart.html',
+        nav=image_list
+    )
+
+@cartbp.route('/checkout_cart', methods=['GET'])
+def checkout():
+    email = session['email']
+    a = get_one_shopping_cart(email)["product_id_list"]
+    for aitem in a:
+        remove_product_from_shopping_cart(aitem, user_email=email)
+    return render_template(
+        'user_cart.html',
+        nav=[]
+    )
 
 @cartbp.route('/guest', methods=['GET'])
 def redirect():
     return render_template('guest_cart.html')
-
-@cartbp.route('/showcart', methods=['POST'])
-
-def showcart():
-    account = request.files.get('id')
-    nav = json.loads(get_one_shopping_cart(account).decode())
-    return render_template(
-        'user_cart.html',
-        nav=nav
-
-    )
-@cartbp.route('/checkinfor', methods=['POST'])
-def checkinfor():
-
-    account = request.form.get('id')
-    print(account)
-
-    infor = json.loads(get_one_user(account).decode())
-    return infor
 
 
 
