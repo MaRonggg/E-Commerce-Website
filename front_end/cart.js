5b73ac74fc3b7c17a6fa484f32b2f41f9ed1276e
MaRonggg <74996996+MaRonggg@users.noreply.github.com>
2022-11-28 22:33:27 -0500

    Update cart.js

--- front_end/cart.js
+++ front_end/cart.js
@@ -2,22 +2,25 @@ function user_cart(){
     $.get('/displaycart',function (res){
         const cartDisplay = document.getElementById('items');
         let total = 0
-        for (const product of res) {
-
+        let temp_contain =""
+        for (const product of res){
             const info = '<br/>' + 'Product: ' + product['product_name'] + '<br/>' +
                 'Price: ' + product['product_price'] + '<br/>';
             total = total+ parseInt(product['product_price'])
-            cartDisplay.innerHTML += '<button onclick="delete_from_cart(' + product['_id'] + ');"><img src="/' + product['product_images'] + '" width="300" height="300">' + info + '</button>';
+            temp_contain = temp_contain + '<button onclick="delete_from_cart(' + product['_id'] + ');"><img src="/' + product['product_image'] + '" width="300" height="300">' + info + '</button>';
+
         }
-        cartDisplay.innerHTML+= '<br/>'+'Total: '+ total +'<br/>'
-        cartDisplay.innerHTML += '<button onclick="checkout();"> </button>';
+        temp_contain= temp_contain+ '<br/>'+'Total: '+ total +'<br/>' +'<button onclick="checkout();">checkout</button>';
+        cartDisplay.innerHTML = temp_contain
     })
 }
 
 function delete_from_cart(product_id) {
         const formData = new FormData();
         formData.append('delete_id', product_id);
+        let total = 0
         const new_display = document.getElementById('items');
+        let temp_contain =""
       $.ajax({
             url: '/delete_from_cart',
             method: 'POST',
@@ -25,23 +28,24 @@ function delete_from_cart(product_id) {
             contentType: false,
             processData: false,
             success: function (res) {
-                let totalprice = 0
-                for (const product of res) {
 
+                for (const product of res) {
                 const info = '<br/>' + 'Product: ' + product['product_name'] + '<br/>' +
                 'Price: ' + product['product_price'] + '<br/>';
-                totalprice = totalprice+ parseInt(product['product_price'])
-                new_display.innerHTML += '<button onclick="delete_from_cart(' + product['_id'] + ');"><img src="/' + product['product_images'] + '" width="300" height="300">' + info + '</button>';
-                }
-                new_display.innerHTML+= '<br/>'+'Total: '+ totalprice +'<br/>'
-                new_display.innerHTML += '<button onclick="checkout();"> </button>';
+                total = total+ parseInt(product['product_price'])
+
+                temp_contain = temp_contain+ '<button onclick="delete_from_cart(' + product['_id'] + ');"><img src="/' + product['product_image'] + '" width="300" height="300">' + info + '</button>';
+
+        }
+        temp_contain= temp_contain+ '<br/>'+'Total: '+ total +'<br/>' +'<button onclick="checkout();">checkout</button>';
+        new_display.innerHTML = temp_contain
             }
         })
 }
 function addToCart(product_id){
      const formData = new FormData();
      formData.append('add_id', product_id);
-     const add_display = document.getElementById('items');
+
       $.ajax({
             url: '/add_to_cart',
             method: 'POST',
@@ -49,16 +53,7 @@ function addToCart(product_id){
             contentType: false,
             processData: false,
             success: function (res) {
-                let totalprice = 0
-                for (const product of res) {
-
-                const info = '<br/>' + 'Product: ' + product['product_name'] + '<br/>' +
-                'Price: ' + product['product_price'] + '<br/>';
-                totalprice = totalprice+ parseInt(product['product_price'])
-                add_display.innerHTML += '<button onclick="delete_from_cart(' + product['_id'] + ');"><img src="/' + product['product_images'] + '" width="300" height="300">' + info + '</button>';
-                }
-                add_display.innerHTML+= '<br/>'+'Total: '+ totalprice +'<br/>'
-                add_display.innerHTML += '<button onclick="checkout();"> </button>';
+              alert(res)
             }
         })
 
@@ -66,11 +61,10 @@ function addToCart(product_id){
 function checkout(){
     $.get('/checkout_cart',function (res){
         const check_out = document.getElementById('items');
-        check_out.innerHTML+= "<br/>"+res
+        check_out.innerHTML= "<br/>"+res
     })
 
 }
-
 
 
 
