1e93d00d49ac67c9281aeacf98aee54e2ca975c1
MaRonggg <74996996+MaRonggg@users.noreply.github.com>
2022-11-28 22:32:58 -0500

    Update cart.py

--- user_data/cart.py
+++ user_data/cart.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, render_template
 import html
 from database.database import get_one_user, get_one_shopping_cart,create_shopping_cart,get_one_product, remove_product_from_shopping_cart, get_one_order, create_order,add_product_to_order, add_product_to_shopping_cart
 from flask import render_template, session
-
+import database.database as db
 
 cartbp = Blueprint('cart', __name__)
 
@@ -28,6 +28,7 @@ def display():
                 product_list.append(get_one_product(aid))
         return product_list
 
+
 @cartbp.route('/add_to_cart', methods=['POST'])
 def add_item():
     email = session['email']
@@ -35,11 +36,9 @@ def add_item():
         create_shopping_cart(email)
     product_id = int(html.escape(request.form.get('add_id')))
     add_product_to_shopping_cart(product_id, user_email=email)
-    a = get_one_shopping_cart(email)["product_id_list"]
-    product_list = []
-    for aid in a:
-        product_list.append(get_one_product(aid))
-    return product_list
+
+
+    return "added"
 
 
 @cartbp.route('/delete_from_cart', methods=['POST'])
@@ -62,11 +61,32 @@ def checkout():
         create_order(email)
 
     for aid in a:
-        add_product_to_order(aid, email)
+
+        remove_product_from_shopping_cart(aid, user_email=email)
+        buyer_email = session['email']
+        seller_email = None
+        product_id = int(aid)
+        data = db.get_all_sale()
+        flag = False
+        for piece in data:
+            for p_id in piece['on_sale_products']:
+                if p_id == product_id:
+                    seller_email = piece['user_email']
+                    flag = True
+                    break
+            if flag is True:
+                break
+
+        if flag is False:
+            return 'Already Sold'
+
+        db.move_product_on_sale_to_sold(product_id=product_id, user_email=seller_email)
+        if db.get_one_order(user_email=buyer_email) is None:
+            db.create_order(user_email=buyer_email)
+        db.add_product_to_order(product_id=product_id, user_email=buyer_email)
 
     res = "Thank You"
     return res
-
 
 
 
